1eb749123c4631de13fb68d6455787ac25072d91
Gus Person <gperson@ucsc.edu>
2018-11-14 15:43:32 -0800

    Initial Commit

---
+++ RestaurantBillDivider/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantBillDivider
+//
+//  Created by Gus Person on 11/14/18.
+//  Copyright © 2018 Gus Person. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
